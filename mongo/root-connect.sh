a9737bb3203a0347054ffa6d72609bd837fe93b6
Edmund Bennett <bennettedmund@gmail.com>
2024-07-26 20:09:31 +0100

    Updates

--- mongo/root-connect.sh
+++ mongo/root-connect.sh
@@ -1,1 +1,1 @@
-kubectl exec -it -n mongo mongodb-statefulset-0 -- mongo -u root -p --authenticationDatabase admin
+kubectl exec -it -n mongo mongodb-statefulset-0-0 -- mongo -u root -p --authenticationDatabase admin
